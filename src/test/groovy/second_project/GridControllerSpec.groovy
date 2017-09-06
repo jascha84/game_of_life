b9f40bc0b389c6549632c3b16677cd4b6f2a1c10
nija <jascha.nitzschmann@dvinci.de>
2017-09-06 15:57:38 +0200

    add generated tests

---
+++ src/test/groovy/second_project/GridControllerSpec.groovy
@@ -0,0 +1,18 @@
+package second_project
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class GridControllerSpec extends Specification implements ControllerUnitTest<GridController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
---
+++ src/test/groovy/second_project/GridServiceSpec.groovy
@@ -0,0 +1,18 @@
+package second_project
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class GridServiceSpec extends Specification implements ServiceUnitTest<GridService>{
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
